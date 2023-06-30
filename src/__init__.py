1894d22e929cd92029162bc569e30b3002c9af41
gabriel-doriath-dohler <gabriel.doriath.dohler@ens.fr>
2023-06-30 23:59:59 +0000

    feat: schematic generation

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,34 @@
-import litemapy
+from litemapy import BlockState, Region, Schematic
 
+print("Hello")
 
-print('Hello')
+# Shortcut to create a schematic with a single region
+reg = Region(0, 0, 0, 21, 21, 21)
+schem = reg.as_schematic(
+    name="Planet", author="SmylerMC", description="Made with litemapy"
+)
+
+# Create the block state we are going to use
+block = BlockState("minecraft:light_blue_concrete")
+
+# Build the planet
+for x, y, z in reg.allblockpos():
+    if round(((x - 10) ** 2 + (y - 10) ** 2 + (z - 10) ** 2) ** 0.5) <= 10:
+        reg.setblock(x, y, z, block)
+
+# Save the schematic
+schem.save("planet.litematic")
+
+# Load the schematic and get its first region
+schem = Schematic.load("planet.litematic")
+reg = list(schem.regions.values())[0]
+
+# Print out the basic shape
+for x in reg.xrange():
+    for z in reg.zrange():
+        b = reg.getblock(x, 10, z)
+        if b.blockid == "minecraft:air":
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
